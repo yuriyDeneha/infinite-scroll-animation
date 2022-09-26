ddbdca2847daaf180ac370b4a1d5e4ef01b7b9da
yuriyDeneha <yapco3333@gmail.com>
2022-09-26 14:03:41 +0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 
 const nisPackage = require('../../package.json');
+declare const chance;
 
 @Component({
   selector: 'my-app',
@@ -10,52 +11,42 @@ const nisPackage = require('../../package.json');
 })
 export class AppComponent {
   array = [];
-  sum = 20;
-  throttle = 300;
+  sum = 30;
+  addOnScroll = 5;
+
+  throttle = 100;
   scrollDistance = 1;
-  scrollUpDistance = 2;
+  scrollUpDistance = 1;
   direction = '';
   modalOpen = false;
 
   nisVersion = nisPackage.dependencies['ngx-infinite-scroll'];
 
   constructor() {
-    this.appendItems(0, this.sum);
+    this.addItems('push');
   }
 
-  addItems(startIndex, endIndex, _method) {
+  addItems(_method) {
     for (let i = 0; i < this.sum; ++i) {
       this.array[_method]([i, ' ', this.generateWord()].join(''));
     }
   }
 
-  appendItems(startIndex, endIndex) {
-    this.addItems(startIndex, endIndex, 'push');
-  }
-
-  prependItems(startIndex, endIndex) {
-    this.addItems(startIndex, endIndex, 'unshift');
-  }
-
   onScrollDown(ev) {
-    console.log('scrolled down!!', ev);
-
-    // add another 20 items
-    const start = this.sum;
-    this.sum += 5;
-    this.appendItems(start, this.sum);
+    console.log('DOWN');
+    this.sum += this.addOnScroll;
+    this.addItems('push');
 
     this.direction = 'down';
   }
 
   onUp(ev) {
-    console.log('scrolled up!', ev);
-    const start = this.sum;
-    this.sum += 20;
-    this.prependItems(start, this.sum);
-
-    this.direction = 'up';
+    // console.log('UP');
+    // this.sum += this.addOnScroll;
+    // this.addItems('unshift');
+    // this.direction = 'up';
   }
+
   generateWord() {
     return chance.word();
   }
