58ee5642a397eb393187070601a669d6aed06d4e
yuriyDeneha <yapco3333@gmail.com>
2022-09-26 13:46:13 +0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { ModalComponent } from './modal/modal.component';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 declare const chance;
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, InfiniteScrollModule ],
-  declarations: [ AppComponent, HelloComponent, ModalComponent ],
+  declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
